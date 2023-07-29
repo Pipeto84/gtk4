3a5c64b3296ff31f4b886fe38e9de8f039f25b8f
Pipeto84 <pipejarauribeboteroa@gmail.com>
2023-07-29 11:13:44 -0500

    composite template 6

--- src/window/imp.rs
+++ src/window/imp.rs
@@ -8,11 +8,11 @@ use std::cell::Cell;
 #[derive(CompositeTemplate,Default)]
 #[template(resource="/org/gtk_rs/example/window.ui")]
 pub struct Window{
-    #[template_child]
-    pub button1: TemplateChild<CustomButton>,
+    // #[template_child]
+    // pub button1: TemplateChild<CustomButton>,
     pub number1: Cell<i32>,
-    #[template_child]
-    pub button2: TemplateChild<CustomButton>,
+    // #[template_child]
+    // pub button2: TemplateChild<CustomButton>,
     pub number2: Cell<i32>,
 }
 #[glib::object_subclass]
@@ -21,6 +21,7 @@ impl ObjectSubclass for Window {
     type Type = super::Window;
     type ParentType = gtk::ApplicationWindow;
     fn class_init(klass: &mut Self::Class) {
+        CustomButton::ensure_type();
         klass.bind_template();
         klass.bind_template_callbacks();
     }
@@ -38,7 +39,7 @@ impl Window {
     }
     #[template_callback]
     fn handle_button2_clicked(&self,button:&CustomButton) {
-        let aumentar=self.number2.get()+1;
+        let aumentar=self.number2.get()+2;
         self.number2.set(aumentar);
         button.set_label(&aumentar.to_string());
     }
